ed15f863fa5e8bd35bc0a3e7c4eab0cbfe9eec3f
Erfan AmanAbadi <erfan80amani23@gmail.com>
2023-07-06 15:31:11 +0330

    not show signout button when user not auth

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,12 +1,14 @@
 "use client";
 
-import { signOut } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import React from "react";
 import SignOut from "./Icons/SignOut";
 import ThemeToggle from "./ThemeToggle";
 
 const Header = () => {
+  const session = useSession();
+
   const signOutHandler = async () => {
     signOut();
   };
@@ -22,9 +24,11 @@ const Header = () => {
       <div className="flex items-center gap-4 fill-neutral-800 dark:fill-neutral-50">
         <ThemeToggle />
 
-        <button onClick={signOutHandler}>
-          <SignOut className="fill-inherit" />
-        </button>
+        {session.status === "authenticated" && (
+          <button onClick={signOutHandler}>
+            <SignOut className="fill-inherit" />
+          </button>
+        )}
       </div>
     </div>
   );
