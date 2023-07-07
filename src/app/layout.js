2078394ff54ea9eafc10d93a267466416e64c378
Erfan AmanAbadi <erfan80amani23@gmail.com>
2023-07-07 20:06:19 +0330

    chnage bg color in dark mode

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,7 +16,7 @@ export default function RootLayout({ children }) {
       <ThemeProvider>
         <body className={inter.className}>
           <Provider>
-            <div className="flex justify-center items-center w-screen h-screen bg-gradient-to-br from-blue-200 dark:from-blue-950 to-purple-400 dark:to-violet-950">
+            <div className="flex justify-center items-center w-screen h-screen bg-gradient-to-br from-blue-200 dark:from-blue-950 to-purple-400 dark:to-neutral-950">
               <div className="flex flex-col bg-gray-200 dark:bg-gray-800 rounded-2xl px-4 py-5 sm:px-10 sm:py-6 lg:px-12 text-sm sm:taxt-base h-5/6 w-10/12 sm:w-2/3 md:w-3/5 lg:w-2/5 xl:w-1/3 shadow-xl gap-8 font-mono">
                 {children}
               </div>
